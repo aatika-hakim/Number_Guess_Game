12e37f70167881ac8d5f9e19d776dea3c83ad19c
Aatika hakim <aatikahakim90@gmail.com>
2023-10-11 19:26:48 +0500

    chalk-animation added

--- index.ts
+++ index.ts
@@ -1,42 +1,44 @@
-#! /usr/bin/env node 
+#!/usr/bin/env node
 
 import inquirer from 'inquirer';
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
 
-interface Answers {
-  guess: string;
-}
+const SecretNum = Math.floor(Math.random() * 100) + 1;
 
-const secretNumber = Math.floor(Math.random() * 100) + 1;
-let attempts = 0;
+const rainbow = chalkAnimation.rainbow('\n --------------- Welcome to Number Guess Game ---------------  \n');
+rainbow.start();
 
-function UserPrompt(){
-    inquirer.prompt({
-        type: "input",
-        name: "guess",
-        message: "Guess the secret Number (1-100): ",
-        validate: (input: string) => {
-            const number = parseInt(input);
-            if(isNaN(number) || number < 1 || number > 100){
-                return "Please enter a valid number between 1 to 100.";
+function Game() {
+    inquirer
+        .prompt({
+            type: 'input',
+            name: 'guess',
+            message: 'Guess the secret Number [1-100]: ',
+            validate: (input) => {
+                const number = parseInt(input);
+                if (isNaN(number) || number < 1 || number > 100) {
+                    return 'Please enter a number between 1 to 100.';
+                }
+                return true;
             }
-            return true;
-        }
-    })
-    .then((answers : Answers)=> {
-        const userGuess = parseInt(answers.guess);
-        attempts++ ;
+        })
+        .then((answers) => {
+            const userGuess = parseInt(answers.guess);
 
-        if(userGuess === secretNumber){
-            console.log(`Congratulatios! You have guessed the Secret Number ${secretNumber} in ${attempts} attempts`);
-        }else if(userGuess < secretNumber){
-            console.log('Try a higher number.');
-            UserPrompt();
-        }else{
-            console.log('Try a lower number.');
-            UserPrompt();
-        }
-    })
+            if (userGuess === SecretNum) {
+                console.log(chalk.blue(`\nCongratulations! You have guessed the Secret Number ${SecretNum}.`));
+            } else if (userGuess < SecretNum) {
+                console.log(chalk.grey('Please try a higher number.'));
+                Game();
+            } else {
+                console.log(chalk.grey('Please try a lower number.'));
+                Game();
+            }
+        });
 }
 
-console.log('Welcome to Number Guessing Game!');
-UserPrompt();
+setTimeout(() => {
+    rainbow.stop();
+    Game();
+}, 2000);
